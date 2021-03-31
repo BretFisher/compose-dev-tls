88fc6a199fe6e596bf110ebab4276094f94dc389
Bret Fisher <bret@bretfisher.com>
2021-03-31 17:02:46 -0400

    Updated cert creation
    now it won't overrite cert files if they exist, and can create a single cert for localhost + custom domain

--- cert.sh
+++ cert.sh
@@ -3,30 +3,50 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Creates a self-signed wildcard cert for local test and dev
+# it always adds localhost and *.localhost, but can also add
+# any domain you add to CLI
 
-# EXAMPLE: ./cert.sh something.com
+# EXAMPLE for *.localhost only: ./cert.sh
+# EXAMPLE for *.localhost plus *.something.com: ./cert.sh something.com
 
-# three files are created: 
-# something.com.key - Secret key good for proxy configs
-# something.com.crt - Public cert good for proxy configs
-# something.com.pem - Combo of those two good for browser/OS import
+# three files are created:
+# cert.key - Secret key good for proxy configs
+# cert.crt - Public cert good for proxy configs
+# cert.pem - Combo of those two good for browser/OS import
 
-DOMAIN_NAME=$1
+if [ ! -f cert.key ]; then
 
-openssl req \
-  -newkey rsa:2048 \
-  -x509 \
-  -nodes \
-  -keyout "$DOMAIN_NAME.key" \
-  -new \
-  -out "$DOMAIN_NAME.crt" \
-  -subj "/CN=*.$DOMAIN_NAME" \
-  -reqexts SAN \
-  -extensions SAN \
-  -config <(cat /etc/ssl/openssl.cnf \
-  <(printf "[SAN]\nsubjectAltName=DNS:*.%s, DNS:%s" "$DOMAIN_NAME" "$DOMAIN_NAME")) \
-  -sha256 \
-  -days 3650
+  DOMAIN_NAME=$1
 
-cat "$DOMAIN_NAME.crt" "$DOMAIN_NAME.key" > "$DOMAIN_NAME.pem"
+  if [ -n "$1" ]; then
+    echo "You supplied domain $1"
+    SAN_LIST="[SAN]\nsubjectAltName=DNS:localhost, DNS:*.localhost, DNS:*.$DOMAIN_NAME, DNS:$DOMAIN_NAME"
+    printf $SAN_LIST
+  else
+    echo "No additional domains will be added to cert"
+    SAN_LIST="[SAN]\nsubjectAltName=DNS:localhost, DNS:*.localhost"
+    printf $SAN_LIST
+  fi
 
+  openssl req \
+    -newkey rsa:2048 \
+    -x509 \
+    -nodes \
+    -keyout "cert.key" \
+    -new \
+    -out "cert.crt" \
+    -subj "/CN=compose-dev-tls Self-Signed" \
+    -reqexts SAN \
+    -extensions SAN \
+    -config <(cat openssl.cnf <(printf $SAN_LIST)) \
+    -sha256 \
+    -days 3650
+
+  cat "cert.crt" "cert.key" > "cert.pem"
+  echo "new TLS self-signed certificate created"
+
+else
+
+  echo "certificate files already exist. Skipping"
+
+fi
